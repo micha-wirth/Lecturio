9cccec2241f10b9d35e8680f33bf4223c960ed3e
micha-wirth <mchwrth@outlook.de>
2017-12-15 20:43:11 +0100

    Tutorial

--- Classes.py
+++ Classes.py
@@ -35,3 +35,25 @@ c3 = Car('Fiat')
 print(c3.brand, c3.color)
 c4 = Car('Audi')
 print(c3.brand, c3.color)
+
+
+class Truck(Car):
+    def drive(self, km):
+        self.km += 2 * km
+
+    def __add__(self, other):
+        return Truck(self.brand + other.brand, self.color + other.color)
+
+
+
+t1 = Truck('MAN', 'white')
+print(t1.brand, t1.color, t1.km)
+t1.drive(50)
+print(t1.brand, t1.color, t1.km)
+
+print(Truck.mro())
+
+t2 = Truck('MAN')
+t3 = Truck('Scania', 'black')
+t4 = t2 + t3
+print(t4.brand, t4.color)
