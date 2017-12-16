fac3b0476c9bd9c14c588e839b8b773a75159573
micha-wirth <mchwrth@outlook.de>
2017-12-16 11:19:10 +0100

    Tutorial

---
+++ example.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Main program.
+"""
+
+
+import  datetime
+from optparse import OptionParser
+import os
+import sys
+
+my_dir = os.path.dirname
+sys.path.append(my_dir(my_dir(os.path.abspath(__file__))))
+
+
+def main():
+    """
+    Main function of main program.
+    :return:
+    """
+    today = datetime.datetime.today()
+    return today
+
+
+if __name__ == '__main__':
+    # Only executed condition is fullfilled.
+    main()
+
+
+
